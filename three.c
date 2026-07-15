8a3c0b39fec2da62225d1e4a890f3703a7098e68
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-15 19:42:08 +0530

    three

---
+++ three.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ( ) {
+
+    int n = 5 ;
+
+    for(int i = 1 ; i <= 2*n-1;  i++){
+        int star = i;
+        if(star > n ){
+            star = 2 * n  - i ;
+        }
+        for(int j = 1 ; j <=star ; j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
