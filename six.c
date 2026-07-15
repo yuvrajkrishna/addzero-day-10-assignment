4707ed8b8fe08615cf41d064c0bfb1c0700bdaa3
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-15 21:21:06 +0530

    six sir's approach

--- six.c
+++ six.c
@@ -1,18 +1,32 @@
 #include<stdio.h>
-int main( ) {
-    int n = 5 ;
-    for(int i = 1 ; i <= 2*n - 1 ; i++){
-        int star = i ;
-        if(star > n){
-            star = 2*n - i;
-        }
-        for(int j = 1 ; j<star;j++){
+
+int main() {
+
+    int n = 5;
+
+    // Upper Half
+    for(int i = 1; i <= n; i++) {
+
+        for(int j = 1; j < i; j++)
             printf(" ");
-        }
-        for(int j = 1 ; j<=2*(n-star)+1;j++){
+
+        for(int j = 1; j <= 2*(n-i)+1; j++)
             printf("*");
-        }
+
         printf("\n");
     }
+
+    // Lower Half
+    for(int i = 2; i <= n; i++) {
+
+        for(int j = 1; j < n-i+1; j++)
+            printf(" ");
+
+        for(int j = 1; j <= 2*i-1; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
     return 0;
 }
