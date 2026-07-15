776b03003d91c246e85844961d11286ba31f5d20
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-15 20:09:23 +0530

    five

--- five.c
+++ five.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
-int main ( ) {
 
-    int n = 5 ;
-    int row = n ;
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1; j <=n-i; j++){
+int main() {
+
+    int n = 5;
+
+    // Upper Half
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++)
             printf(" ");
-        }
-        for(int j = 1; j <= 2*i-1; j++){
+
+        for(int j = 1; j <= 2 * i - 1; j++)
             printf("*");
-        }
+
         printf("\n");
     }
-    n--;
-    while(n>0){
-        for(int i = n ; i >= 1 ; i--){
-        for(int j = 1; j <=row-n; j++){
+
+    // Lower Half
+    for(int i = n - 1; i >= 1; i--) {
+
+        for(int j = 1; j <= n - i; j++)
             printf(" ");
-        }
-        for(int j = 1; j <= 2*i-1; j++){
+
+        for(int j = 1; j <= 2 * i - 1; j++)
             printf("*");
-        }
+
         printf("\n");
-        n--;
-    }
     }
 
-    return 0 ;
-
+    return 0;
 }
