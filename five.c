aa3d2666b3aadf365c32374ba150ab491b707117
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-15 20:14:48 +0530

    five

--- five.c
+++ five.c
@@ -4,28 +4,19 @@ int main() {
 
     int n = 5;
 
-    // Upper Half
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n - i; j++)
+    for(int i = 1 ; i <= 2*n - 1; i++){
+        int star = i;
+        if(star > n){
+            star = 2*n - i;
+        }
+        for(int j = 1 ; j <= n - star ; j++){
             printf(" ");
-
-        for(int j = 1; j <= 2 * i - 1; j++)
+        }
+        for(int j = 1 ; j <=star*2 -1 ; j++){
             printf("*");
-
+        }
         printf("\n");
     }
-
-    // Lower Half
-    for(int i = n - 1; i >= 1; i--) {
-
-        for(int j = 1; j <= n - i; j++)
-            printf(" ");
-
-        for(int j = 1; j <= 2 * i - 1; j++)
-            printf("*");
-
-        printf("\n");
-    }
-
+    
     return 0;
 }
